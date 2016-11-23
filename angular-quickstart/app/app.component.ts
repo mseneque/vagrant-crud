9a9f22e861da373a960a3e047ebb048edbb374c3
Matthew Seneque <mattseneque@gmail.com>
2016-11-23 08:09:56 +0000

    remove tutorials

--- angular-quickstart/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-// execute dev tests with python -m SimpleHTTPServer
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>Hello Angular 2!</h1>'
-})
-export class AppComponent { }
